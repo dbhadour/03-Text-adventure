508c9b7027b1ec7d9023339eff7168f14c48a8fb
dbhadour <deviu2022@gmail.com>
2019-09-19 09:43:28 -0400

    submitting porject03

--- main.py
+++ main.py
@@ -1,58 +1,55 @@
-import sys, logging, json
+import sys, os, json
 
-#check to make sure we are running the right version of Python
 version = (3,7)
 assert sys.version_info >= version, "This script requires at least Python {0}.{1}".format(version[0],version[1])
 
-#turn on logging, in case we have to leave ourselves debugging messages
-logging.basicConfig(format='[%(filename)s:%(lineno)d] %(message)s', level=logging.DEBUG)
-logger = logging.getLogger(__name__)
 
-def render(world,current_location):
-    ''' Print out desription of the current location '''
-    room = world[current_location]
-    print(room["name"])
-    print(room["desc"])
-    return True
 
-def check_input():
-    '''Request input from player, validate input '''
-    user_input = input("What would you like to do?")
-    # do we want to validate?
-    return user_input
 
-def update(world, current, selection):
-    game(current)("exits")
-    for e in game ["room"][current]["exits"]
-    if e['verb'] == selection:
-        if e['target' == 'No Exit']:
-            print("You can't go that way")
-            else current = e('target'):
-    '''Check if we need to move to a new location, etc. '''
+# Game loop functions
+def render(game,current):
+    ''' Displays the current room '''
 
+    print('You are in the ' + game['rooms'][current]['name'])
+    print(game['rooms'][current]['desc'])
+
+def getInput():
+    ''' Asks the user for input and returns a stripped, uppercase version of what they typed '''
+
+    response = input('What would you like to do? ').strip().upper()
+    return response
+
+
+def update(response,game,current):
+    ''' Process the input and update the state of the world '''
+    for e in game['rooms'][current]['exits']:
+        if response == e['verb']:
+            current = e['target']
     return current
 
+
+
 def main():
+
     game = {}
     with open('zork.json') as json_file:
         game = json.load(json_file)
-    # Your game goes here!
 
-    current = 'WHOUS'
+    current = 'Barrack'
 
     quit = False
 
     while not quit:
-        #Render world
-        render(game["rooms"],current)
-        #check for player input
-        user_input = check_input()
-        #update state of the world
 
-    return True
+        render(game,current)
+        response = getInput()
+        current = update(response,game,current)
+
+        if response == 'QUIT':
+            quit = True
 
 
 
-#if we are running this from the command line, run main
+
 if __name__ == '__main__':
 	main()
